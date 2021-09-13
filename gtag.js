e910e040d7b3e5eda22c81ef1f4de6b1647a7c7a
Gul Inan <56047198+gulinan@users.noreply.github.com>
2021-09-13 20:03:41 +0300

    first

---
+++ gtag.js
@@ -0,0 +1,10 @@
+{literal}
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-179590858-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-179590858-1');
+</script>
+{/literal}
